769bf43d48d7a95b1336af2c7bdb5b71e4b1f495
Samantha <smf5583@gmail.com>
2021-02-28 22:19:08 -0500

    Add functionality to movie grid collection view

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Samantha Floyd on 2/28/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+}
